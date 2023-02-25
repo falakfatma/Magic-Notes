c5a6d4443dd3829298ef3e7f0238fdb1f8bfddc1
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2023-02-25 18:21:57 +0000

    arrange code .

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const add_Btn = document.getElementById('addBtn')
-add_Btn.addEventListener('click',()=>{
+add_Btn.addEventListener('click', () => {
   // <!--       <div class="note">
-        // <div class="navigator"></div>
-        // <textarea></textarea>
-      // </div> -->
+  // <div class="navigator"></div>
+  // <textarea></textarea>
+  // </div> -->
   let ele = document.createElement('div')
-   ele.classList.add('note')
-  let NewEle = document.createElement('div')
-  NewEle.classList.add('navigator')
-  ele.appendChild(NewEle);
-  let  textarea = document.createElement('textarea')
-  ele.appendChild(textarea);
+  ele.classList.add('note')
+  ele.innerHTML =
+` 
+      <div class="navigator"></div>
+      <textarea></textarea>
+`
   document.querySelector('.container').appendChild(ele)
-  console.log(ele.classList)
+  // console.log(ele.classList)
 })
