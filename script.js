d5d1d3dbd058289c7044479d5b2c6ccb5b34a191
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2023-02-28 08:27:35 +0000

    Delete Notes on Click Delete Button

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const add_Btn = document.getElementById('addBtn')
-const delete_Btn = document.querySelector('.trash')
+const textarea = document.querySelector('textarea')
 const note = document.querySelector('.note')
-let ele = document.createElement('div')
+const save_Btn = document.querySelector('.save')
+// let ele = document.createElement('div')
 
 // add Notes
 add_Btn.addEventListener('click', () => {
-  let ele = document.createElement('div')
-  ele.classList.add('note')
-  ele.innerHTML =
+  let note = document.createElement('div')
+  note.classList.add('note')
+  note.innerHTML =
     `
       <div class="navigator">
         <i class="save fa-solid fa-floppy-disk"></i>
@@ -15,14 +16,33 @@ add_Btn.addEventListener('click', () => {
       </div>
       <textarea></textarea>
 `
-  document.querySelector('.container').appendChild(ele)
+    note.querySelector('.trash').addEventListener('click',()=>{
+    note.remove()
+  })
+    note.querySelector('.save').addEventListener('click',()=>{
+    let tasks = localStorage()
+    // note.remove()
+  })
+  document.querySelector('.container').appendChild(note)
 })
-// delete Notes
-delete_Btn.addEventListener('click', (e) => {
-  console.log(delete_Btn)
-  console.log(ele)
-  // ele.classList.add('note')
-  ele.remove()
-  document.querySelector('.container').appendChild(ele)
 
-})
+//Save Note 
+// save_Btn.addEventListener('click', () => {
+//   console.log(textarea.value)
+//     localStorage.setItem(textarea.value,`${textarea.value}`)
+// })
+
+
+// // delete Notes
+function myFun(e){
+    let tasks = localStorage.key(e);
+  let deletedListItem = document.getElementById(tasks);
+  deletedListItem.remove();
+  localStorage.removeItem(objectKey);
+}
+//   // note.classList.add('note')
+//   console.log(delete_Btn)
+//   console.log(ele)
+//   ele.remove()
+//   document.querySelector('.container').appendChild(ele)
+
